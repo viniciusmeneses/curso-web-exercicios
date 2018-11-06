f095217a4cbe67845b8ac905ba2e781fe2c875bc
vini23hot <vini23hot@gmail.com>
2018-11-06 18:41:28 -0200

    Created first component

--- Exercícios/20 - React/exercicios/src/index.js
+++ Exercícios/20 - React/exercicios/src/index.js
@@ -2,11 +2,17 @@ import React from 'react'
 // Somente no index.js vamos importar o react-dom
 import ReactDOM from 'react-dom'
 
+// Importando o componente criado
+import Primeiro from './components/Primeiro'
+
+
 // <h1>React 2</h1> - É um código em JSX que será feito o transpile para JS puro para funcionar no browser
 // const elemento = <h1>React 2</h1>
 
 // React-dom conversa diretamente com a DOM do browser
 
-// Renderiza o primeiro componente/HTML do React através do react-dom
+// Renderiza o primeiro componente do React através do react-dom
 // Passando o componente e o elemento em que ele vai ser inserido dentro
-ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
+
+// O componente importado pode ser acessado com JSX utilizando como se fosse tag HTML
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
