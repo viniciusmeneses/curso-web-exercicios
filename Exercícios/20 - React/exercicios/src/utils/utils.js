2bc2d4e53ccea6fb2c15046a95f9519409350126
vini23hot <vini23hot@gmail.com>
2018-11-09 14:13:55 -0200

    Added comment to utils file

--- Exercícios/20 - React/exercicios/src/utils/utils.js
+++ Exercícios/20 - React/exercicios/src/utils/utils.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+// Exporta uma função que recebe as propriedades do componente
+// e faz um map nos componentes filhos passados dentro do componente pai
+// clonando cada filho e passando para ele as propriedades do pai (como se fosse herança)
+// mais as propriedades do filho
 export function childrenWithProps(props) {
   return React.Children.map(props.children, child => {
     return React.cloneElement(child, {
